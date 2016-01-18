58f759fc4de29f6443be9c3f7fbad8bf0e637977
christiandusse <christian@dusse.eu>
2016-01-18 12:03:40 +0100

    Bound shopping cart to backend

--- ribay-client/app/cart/cart.js
+++ ribay-client/app/cart/cart.js
@@ -13,28 +13,29 @@ angular.module('myApp.cart', ['ngRoute', 'ngAnimate', 'ui.bootstrap'])
         });
     }])
 
-    .controller('cartCtrl', ['$scope', function ($scope) {
+    .controller('cartCtrl', ['$scope', '$http', function ($scope, $http) {
 
-        // TODO get data from backend rest api
-        $scope.cart = [{
-            id: "abc",
-            image: "http://placehold.it/200x400",
-            name: "Name of article",
-            price: 12.34,
-            quantity: 2
-        }, {
-            id: "def",
-            image: "http://placehold.it/400x200",
-            name: "Other article",
-            price: 5.99,
-            quantity: 1
-        }, {
-            id: "ghi",
-            image: "http://placehold.it/50x50",
-            name: "Third article",
-            price: 1.99,
-            quantity: 10
-        }];
+        $scope.cart = undefined;
+        $scope.subtotal = undefined;
+
+        $http.get('http://localhost:8080/cart/get/').success(function (data) {
+            $scope.cart = data.articles;
+
+            var total_amount = 0;
+            angular.forEach($scope.cart, function (item) {
+                total_amount += item.quantity;
+            });
+
+            var total_price = 0;
+            angular.forEach($scope.cart, function (item) {
+                total_price += (item.quantity * item.price);
+            });
+
+            $scope.subtotal = {
+                amount: total_amount,
+                price: total_price
+            };
+        });
 
         $scope.deleteItem = function (item) {
             // TODO delete item from cart
@@ -44,21 +45,6 @@ angular.module('myApp.cart', ['ngRoute', 'ngAnimate', 'ui.bootstrap'])
         $scope.setQuantity = function (item, quantity) {
             // TODO set quantity of item
             alert("set quantity of " + item.id + " to " + quantity);
-        };
-
-        var amount = 0;
-        angular.forEach($scope.cart, function (item) {
-            amount += item.quantity;
-        });
-
-        var price = 0;
-        angular.forEach($scope.cart, function (item) {
-            price += (item.quantity * item.price);
-        });
-
-        $scope.subtotal = {
-            amount: amount,
-            price: price
         };
 
         $scope.checkout = function () {
