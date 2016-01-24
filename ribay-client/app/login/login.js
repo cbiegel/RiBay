f9c84aeaf82ae0ff40578394b263a43aa1e8e7da
christiandusse <christian@dusse.eu>
2016-01-24 15:46:57 +0100

    Introduced LoginService for backend calls and better styling

--- ribay-client/app/login/login.js
+++ ribay-client/app/login/login.js
@@ -13,90 +13,95 @@ angular.module('myApp.login', ['ngRoute', 'ngAnimate', 'ui.bootstrap'])
         });
     }])
 
-    .controller('loginCtrl', ['$rootScope', '$scope', '$location', '$timeout', '$filter', '$q', function ($rootScope, $scope, $location, $timeout, $filter, $q) {
+    .service('loginService', ['$timeout', function ($timeout) {
+
+        this.checkLogin = function (username, password, callback) {
+
+            /* Dummy authentication for testing, uses $timeout to simulate api call
+             ----------------------------------------------*/
+            $timeout(function () {
+                var response = {success: (username === 'test') && (password === 'test')};
+                if (!response.success) {
+                    response.message = 'Username or password is incorrect';
+                }
+                callback(response);
+            }, 1000);
+
+        };
+
+    }])
+
+    .controller('loginCtrl', ['$rootScope', '$scope', '$location', '$filter', '$q', 'loginService', function ($rootScope, $scope, $location, $filter, $q, loginService) {
 
 
-            $scope.login = function () {
-                $scope.dataLoading = true;
-                $scope.checkLogin($scope.username, $scope.password, function (response) {
-                    if (response.success) {
-                        $scope.setCredentials($scope.username, $scope.password);
-                        $location.url('/loginSuccess');
-                    } else {
-                        $scope.error = response.message;
-                        $scope.dataLoading = false;
-                    }
-                });
+        $scope.login = function () {
+            $scope.dataLoading = true;
+            loginService.checkLogin($scope.username, $scope.password, function (response) {
+                if (response.success) {
+                    $scope.setCredentials($scope.username, $scope.password);
+                    $location.url('/loginSuccess');
+                } else {
+                    $scope.error = response.message;
+                    $scope.dataLoading = false;
+                }
+            });
+        };
+
+        $scope.setCredentials = function (username, password) {
+            var authdata = $scope.base64Encode(username + ':' + password);
+
+            $rootScope.globals = {
+                currentUser: {
+                    username: username,
+                    authdata: authdata
+                }
             };
 
-            $scope.checkLogin = function (username, password, callback) {
+            // $http.defaults.headers.common['Authorization'] = 'Basic ' + authdata;
+            // $cookieStore.put('globals', $rootScope.globals);
+        };
 
-                /* Dummy authentication for testing, uses $timeout to simulate api call
-                 ----------------------------------------------*/
-                $timeout(function () {
-                    var response = {success: username === 'test' && password === 'test'};
-                    if (!response.success) {
-                        response.message = 'Username or password is incorrect';
-                    }
-                    callback(response);
-                }, 1000);
-            };
+        $scope.clearCredentials = function () {
+            $rootScope.globals = {};
+            // $cookieStore.remove('globals');
+            // $http.defaults.headers.common.Authorization = 'Basic ';
+        };
 
-            $scope.setCredentials = function (username, password) {
-                var authdata = $scope.base64Encode(username + ':' + password);
+        $scope.base64Encode = function (input) {
+            var keyStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
 
-                $rootScope.globals = {
-                    currentUser: {
-                        username: username,
-                        authdata: authdata
-                    }
-                };
+            var output = "";
+            var chr1, chr2, chr3 = "";
+            var enc1, enc2, enc3, enc4 = "";
+            var i = 0;
 
-                // $http.defaults.headers.common['Authorization'] = 'Basic ' + authdata;
-                // $cookieStore.put('globals', $rootScope.globals);
-            };
+            do {
+                chr1 = input.charCodeAt(i++);
+                chr2 = input.charCodeAt(i++);
+                chr3 = input.charCodeAt(i++);
 
-            $scope.clearCredentials = function () {
-                $rootScope.globals = {};
-                // $cookieStore.remove('globals');
-                // $http.defaults.headers.common.Authorization = 'Basic ';
-            };
+                enc1 = chr1 >> 2;
+                enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+                enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+                enc4 = chr3 & 63;
 
-            $scope.base64Encode = function (input) {
-                var keyStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+                if (isNaN(chr2)) {
+                    enc3 = enc4 = 64;
+                } else if (isNaN(chr3)) {
+                    enc4 = 64;
+                }
 
-                var output = "";
-                var chr1, chr2, chr3 = "";
-                var enc1, enc2, enc3, enc4 = "";
-                var i = 0;
+                output = output +
+                    keyStr.charAt(enc1) +
+                    keyStr.charAt(enc2) +
+                    keyStr.charAt(enc3) +
+                    keyStr.charAt(enc4);
+                chr1 = chr2 = chr3 = "";
+                enc1 = enc2 = enc3 = enc4 = "";
+            } while (i < input.length);
 
-                do {
-                    chr1 = input.charCodeAt(i++);
-                    chr2 = input.charCodeAt(i++);
-                    chr3 = input.charCodeAt(i++);
-
-                    enc1 = chr1 >> 2;
-                    enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-                    enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-                    enc4 = chr3 & 63;
-
-                    if (isNaN(chr2)) {
-                        enc3 = enc4 = 64;
-                    } else if (isNaN(chr3)) {
-                        enc4 = 64;
-                    }
-
-                    output = output +
-                        keyStr.charAt(enc1) +
-                        keyStr.charAt(enc2) +
-                        keyStr.charAt(enc3) +
-                        keyStr.charAt(enc4);
-                    chr1 = chr2 = chr3 = "";
-                    enc1 = enc2 = enc3 = enc4 = "";
-                } while (i < input.length);
-
-                return output;
-            };
+            return output;
+        };
 
         $scope.base64Decode = function (input) {
             var keyStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
@@ -140,5 +145,5 @@ angular.module('myApp.login', ['ngRoute', 'ngAnimate', 'ui.bootstrap'])
             } while (i < input.length);
 
             return output;
-            };
-        }]);
+        };
+    }]);
