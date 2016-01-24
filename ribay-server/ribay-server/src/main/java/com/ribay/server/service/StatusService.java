1e663a103dac970eb004de4868f95e9c08357743
christiandusse <christian@dusse.eu>
2016-01-24 15:40:09 +0100

    Added more information for status page: - buckets include their type and are ordered by name - cluster status is now a sorted map of ips to node status instead of a list of node status

--- ribay-server/ribay-server/src/main/java/com/ribay/server/service/StatusService.java
+++ ribay-server/ribay-server/src/main/java/com/ribay/server/service/StatusService.java
@@ -1,7 +1,11 @@
 package com.ribay.server.service;
 
+import java.math.BigInteger;
+import java.net.InetAddress;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -14,6 +18,7 @@ import org.springframework.web.client.RestTemplate;
 
 import com.basho.riak.client.api.commands.buckets.ListBuckets;
 import com.basho.riak.client.api.commands.kv.ListKeys;
+import com.basho.riak.client.core.query.Location;
 import com.basho.riak.client.core.query.Namespace;
 import com.ribay.server.db.MyRiakClient;
 import com.ribay.server.util.AuthInterceptor;
@@ -31,13 +36,15 @@ public class StatusService
 
     @CrossOrigin(origins = "*", allowedHeaders = "*", exposedHeaders = AuthInterceptor.HEADER_NAME)
     @RequestMapping(path = "/status/db/buckets")
-    public List<String> getBuckets() throws Exception
+    public Map<String, String> getBuckets() throws Exception
     {
         ListBuckets lb = new ListBuckets.Builder("my_type").build();
         ListBuckets.Response lbResp = client.execute(lb);
 
-        List<String> buckets = StreamSupport.stream(lbResp.spliterator(), false)
-                .map((o) -> o.getBucketNameAsString()).collect(Collectors.toList());
+        Map<String, String> buckets = StreamSupport.stream(lbResp.spliterator(), false)
+                .collect(Collectors.toMap(Namespace::getBucketNameAsString,
+                        Namespace::getBucketTypeAsString, ((o1, o2) -> o2),
+                        (() -> new TreeMap<>(String.CASE_INSENSITIVE_ORDER))));
 
         return buckets;
     }
@@ -50,22 +57,51 @@ public class StatusService
         ListKeys.Response lkResp = client.execute(lk);
 
         List<String> keys = StreamSupport.stream(lkResp.spliterator(), false)
-                .map((o) -> o.getKeyAsString()).collect(Collectors.toList());
+                .map(Location::getKeyAsString).collect(Collectors.toList());
         return keys;
     }
 
     @CrossOrigin(origins = "*", allowedHeaders = "*", exposedHeaders = AuthInterceptor.HEADER_NAME)
     @RequestMapping(path = "/status/db/cluster")
-    public List<Object> getClusterStatus() throws Exception
+    public Map<String, Object> getClusterStatus() throws Exception
     {
         // no native api for that. use http instead
 
-        List<Object> result = Arrays.stream(properties.getDatabaseIps()).parallel()
+        Map<String, Object> result = Arrays.stream(properties.getDatabaseIps())
                 .map((ip) -> "http://" + ip + ":8098/stats")
-                .map((url) -> new RestTemplate().getForObject(url, Object.class))
-                .collect(Collectors.toList());
+                .parallel().map(
+                        (url) -> new RestTemplate().getForObject(url, Object.class))
+                .collect(Collectors.toMap(
+                        ((jsonObj) -> (String) ((Map<?, ?>) jsonObj).get("nodename")),
+                        ((jsonObj) -> jsonObj), ((jsonObj1, jsonObj2) -> jsonObj2),
+                        (() -> new TreeMap<>(StatusService::compareNodeNames))));
 
         return result;
+    }
+
+    private static int compareNodeNames(String nodeName1, String nodeName2)
+    {
+        try
+        {
+            // remove 'riak@' prefix
+            String ip1 = nodeName1.split("@")[1];
+            String ip2 = nodeName2.split("@")[1];
+
+            // convert to byte representation
+            InetAddress ia1 = InetAddress.getByName(ip1);
+            InetAddress ia2 = InetAddress.getByName(ip2);
+
+            // convert to number
+            BigInteger value1 = new BigInteger(ia1.getAddress());
+            BigInteger value2 = new BigInteger(ia2.getAddress());
+
+            // compare number
+            return value1.subtract(value2).signum();
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException(e);
+        }
     }
 
 }
